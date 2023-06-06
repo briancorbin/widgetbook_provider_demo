5216c6582a630812eb3baccee0e7419f2046e882
briancorbin <brian.william.corbin@gmail.com>
2023-06-06 13:50:57 -0700

    adding example of WB not working with multiple view model instances

--- test/ui_catalogs.dart
+++ test/ui_catalogs.dart
@@ -2,7 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_provider_exploration/users/user.model.dart';
-import 'package:widgetbook_provider_exploration/users/userDetails/userDetails.view.dart';
 import 'package:widgetbook_provider_exploration/users/users.view.dart';
 import 'package:widgetbook_provider_exploration/users/users.viewModel.dart';
 
@@ -40,6 +39,26 @@ class HotReload extends StatelessWidget {
                             email: wbContext.knobs.string(
                               label: 'Email',
                               initialValue: '',
+                            )),
+                      ],
+                    ),
+                    child: const UsersView(),
+                  ),
+                ),
+                WidgetbookUseCase(
+                  name: 'With View Model 2',
+                  builder: (wbContext) =>
+                      ChangeNotifierProvider<UsersViewModel>(
+                    create: (context) => FakeUsersViewModel(
+                      users: [
+                        UserModel(
+                            name: wbContext.knobs.string(
+                              label: 'Name',
+                              initialValue: 'Mary Jane',
+                            ),
+                            email: wbContext.knobs.string(
+                              label: 'Email',
+                              initialValue: 'mary@email.com',
                             )),
                       ],
                     ),
